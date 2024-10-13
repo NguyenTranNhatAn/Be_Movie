6d16cd719ff27c5233f24bb1f880354fc97cc725
NguyenTranNhatAn <nan22052004@gmail.com>
2024-10-13 10:52:46 +0700

    done 30%

--- module/Movies/MovieModel.js
+++ module/Movies/MovieModel.js
@@ -3,8 +3,8 @@ const Schema= mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const MovieSchema= new Schema({
     name:{type: String, require: true},
-    duration:String,
-    release_date:Date,
+    duration:{ type: Date, default: Date.now() },
+    release_date:{ type: Date, default: Date.now() },
     trailer:String,
     images:[Object],
     description:String,
